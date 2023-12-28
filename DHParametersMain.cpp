ade2fdf7453a706f09e594fc0cd7bb6d367f74b7
FcGod <36422731+FcGod@users.noreply.github.com>
2023-12-28 15:22:20 +0200

    Update DHParametersMain.cpp

--- DHParametersMain.cpp
+++ DHParametersMain.cpp
@@ -3,7 +3,10 @@
 
 int main()
 {
-	Matrix T1, T2, T3, T4, T5, T6, result;
+	/* initialize random seed: */
+	srand(static_cast<unsigned>(time(0)));
+
+	Matrix T1, T2, T3, T4, T5, T6, result, test1, test2;
 
 	//Make Transformed Matrices
 
@@ -13,6 +16,21 @@ int main()
 	T4 = DH::CreateMatrix(1056, 0, 0, 90);
 	T5 = DH::CreateMatrix(0, 0, 0, -90);
 	T6 = DH::CreateMatrix(250, 90, 0, 0);
+	/*
+	DH::printMatrix(T1);
+	DH::printMatrix(T2);
+	DH::printMatrix(T4);
+	DH::printMatrix(T3);
+	DH::printMatrix(T5);
+	DH::printMatrix(T6);
+	
+	test1 = DH::fill();
+	test2 = DH::fill();
+	DH::printMatrix(test1);
+	DH::printMatrix(test2);
+	result = test1 * test2;
+	DH::printMatrix(result);
+	*/
 
 	result = DH::calculateFinal(T1, T2, T3, T4, T5, T6);
 }
